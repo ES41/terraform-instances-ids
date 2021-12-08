0f386f1c19e6d7a47610aa557b0d708d9cfb907e
Eyal159 <74238936+Eyal159@users.noreply.github.com>
2021-12-08 11:33:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "external" "external_resource" {
   program = ["/usr/bin/python3", "${path.module}/list_instances.py"]
 
   query = {
-    cluster_name = "production"
+    cluster_name = "change_me"
   }
 }
 output "output" {
